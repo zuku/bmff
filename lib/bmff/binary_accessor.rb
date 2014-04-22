6eb368f33eb48e81fdea5511fbf492188c5c3afd
Takayuki Ogiso <gomatofu@gmail.com>
2014-04-22 23:05:05 +0900

    Add binary accessor methods.
    BMFF::BinaryAccessor module will have signed/unsigned 8/16/32/64-bit integer converting methods.

--- lib/bmff/binary_accessor.rb
+++ lib/bmff/binary_accessor.rb
@@ -2,7 +2,54 @@
 # vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
 
 module BMFF::BinaryAccessor
+  BYTE_ORDER = "\xFE\xFF".unpack("s").first == 0xFEFF ? :be : :le
+
+  def get_int8
+    sysread(1).unpack("c").first
+  end
+
   def get_uint8
     sysread(1).unpack("C").first
+  end
+
+  def get_int16
+    flip_byte_if_needed(_sysread(2)).unpack("s").first
+  end
+
+  def get_uint16
+    _sysread(2).unpack("n").first
+  end
+
+  def get_int32
+    flip_byte_if_needed(_sysread(4)).unpack("l").first
+  end
+
+  def get_uint32
+    _sysread(4).unpack("N").first
+  end
+
+  def get_int64
+    b1 = flip_byte_if_needed(_sysread(4)).unpack("l").first
+    b2 = _sysread(4).unpack("N").first
+    (b1 << 32) | b2
+  end
+
+  def get_uint64
+    b1, b2 = _sysread(8).unpack("N2")
+    (b1 << 32) | b2
+  end
+
+  private
+  def flip_byte_if_needed(data)
+    if BYTE_ORDER == :le
+      return data.force_encoding("ascii-8bit").reverse
+    end
+    return data
+  end
+
+  def _sysread(size)
+    data = sysread(size)
+    raise EOFError unless data.bytesize == size
+    data
   end
 end
--- test/test_binary_accessor.rb
+++ test/test_binary_accessor.rb
@@ -6,6 +6,23 @@ require 'bmff/binary_accessor'
 require 'stringio'
 
 class TestBMFFBinaryAccessor < MiniTest::Unit::TestCase
+  def test_get_int8
+    io = StringIO.new("\x00\xFF\x80", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(0, io.get_int8)
+    assert_equal(-1, io.get_int8)
+    assert_equal(-128, io.get_int8)
+    assert(io.eof?)
+  end
+
+  def test_get_int8_insufficient_data
+    io = StringIO.new("", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_int8
+    end
+  end
+
   def test_get_uint8
     io = StringIO.new("\x00\xFF\xF0", "r:ascii-8bit")
     io.extend(BMFF::BinaryAccessor)
@@ -13,5 +30,116 @@ class TestBMFFBinaryAccessor < MiniTest::Unit::TestCase
     assert_equal(255, io.get_uint8)
     assert_equal(240, io.get_uint8)
     assert(io.eof?)
+  end
+
+  def test_get_uint8_insufficient_data
+    io = StringIO.new("", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_uint8
+    end
+  end
+
+  def test_get_int16
+    io = StringIO.new("\x00\xFF\xFF\xFF\x80\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(255, io.get_int16)
+    assert_equal(-1, io.get_int16)
+    assert_equal(-32768, io.get_int16)
+    assert(io.eof?)
+  end
+
+  def test_get_int16_insufficient_data
+    io = StringIO.new("\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_int16
+    end
+  end
+
+  def test_get_uint16
+    io = StringIO.new("\x00\xFF\xFF\xFF", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(255, io.get_uint16)
+    assert_equal(65535, io.get_uint16)
+    assert(io.eof?)
+  end
+
+  def test_get_uint16_insufficient_data
+    io = StringIO.new("\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_uint16
+    end
+  end
+
+  def test_get_int32
+    io = StringIO.new("\x00\x00\x00\x00\xFF\xFF\xFF\xFF\x80\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(0, io.get_int32)
+    assert_equal(-1, io.get_int32)
+    assert_equal(-2147483648, io.get_int32)
+    assert(io.eof?)
+  end
+
+  def test_get_int32_insufficient_data
+    io = StringIO.new("\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_int32
+    end
+  end
+
+  def test_get_uint32
+    io = StringIO.new("\x00\x00\x00\x00\xFF\xFF\xFF\xFF\x80\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(0, io.get_uint32)
+    assert_equal(4294967295, io.get_uint32)
+    assert_equal(2147483648, io.get_uint32)
+    assert(io.eof?)
+  end
+
+  def test_get_uint32_insufficient_data
+    io = StringIO.new("\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_uint32
+    end
+  end
+
+  def test_get_int64
+    io = StringIO.new("\x00\x00\x00\x00\x00\x00\x00\x00" + "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" +
+                      "\x80\x00\x00\x00\x00\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(0, io.get_int64)
+    assert_equal(-1, io.get_int64)
+    assert_equal(-9223372036854775808, io.get_int64)
+    assert(io.eof?)
+  end
+
+  def test_get_int64_insufficient_data
+    io = StringIO.new("\x00\x00\x00\x00\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_int64
+    end
+  end
+
+  def test_get_uint64
+    io = StringIO.new("\x00\x00\x00\x00\x00\x00\x00\x00" + "\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF" +
+                      "\x80\x00\x00\x00\x00\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_equal(0, io.get_uint64)
+    assert_equal(18446744073709551615, io.get_uint64)
+    assert_equal(9223372036854775808, io.get_uint64)
+    assert(io.eof?)
+  end
+
+  def test_get_uint64_insufficient_data
+    io = StringIO.new("\x00\x00\x00\x00\x00\x00\x00", "r:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    assert_raises(EOFError) do
+      io.get_uint64
+    end
   end
 end
