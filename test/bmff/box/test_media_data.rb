cfdbfa63c71d10bf2fdb1608548397ee887117ce
Takayuki Ogiso <gomatofu@gmail.com>
2014-05-08 04:28:29 +0900

    Add a test file for BMFF::Box::MediaData.

---
+++ test/bmff/box/test_media_data.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+# vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
+
+require_relative '../../minitest_helper'
+require 'bmff/box'
+require 'stringio'
+
+class TestBMFFBoxMediaData < MiniTest::Unit::TestCase
+  def test_parse
+    io = StringIO.new("", "r+:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    io.write_uint32(0)
+    io.write_ascii("mdat")
+    io.write_byte("abcdefg") # data
+    size = io.pos
+    io.pos = 0
+    io.write_uint32(size)
+    io.pos = 0
+
+    box = BMFF::Box.get_box(io, nil)
+    assert_instance_of(BMFF::Box::MediaData, box)
+    assert_equal(size, box.actual_size)
+    assert_equal("mdat", box.type)
+  end
+end
