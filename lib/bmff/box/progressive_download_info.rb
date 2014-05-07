b5578514568dd98a8689a6dd3ccff4a9e1a21b33
Takayuki Ogiso <gomatofu@gmail.com>
2014-05-08 04:49:45 +0900

    Add a test file for BMFF::Box::ProgressiveDownloadInfo.
    BMFF::Box::ProgressiveDownloadInfo property is changed in order to standardize a property structure.

--- lib/bmff/box/progressive_download_info.rb
+++ lib/bmff/box/progressive_download_info.rb
@@ -2,15 +2,16 @@
 # vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
 
 class BMFF::Box::ProgressiveDownloadInfo < BMFF::Box::Full
-  attr_accessor :information
+  attr_accessor :rate, :initial_delay
   register_box "pdin"
 
   def parse_data
     super
-    @information = []
+    @rate = []
+    @initial_delay = []
     until eob?
-      @information << {:rate => io.get_uint32,
-                       :initial_delay => io.get_uint32}
+      @rate << io.get_uint32
+      @initial_delay << io.get_uint32
     end
   end
 end
