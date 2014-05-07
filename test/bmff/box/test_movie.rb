4dc5084b5334848c6d17aeab11dcc7b40cfac786
Takayuki Ogiso <gomatofu@gmail.com>
2014-05-08 04:56:40 +0900

    Add a test file for BMFF::Box::Movie.

---
+++ test/bmff/box/test_movie.rb
@@ -0,0 +1,26 @@
+# coding: utf-8
+# vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
+
+require_relative '../../minitest_helper'
+require 'bmff/box'
+require 'stringio'
+
+class TestBMFFBoxMovie < MiniTest::Unit::TestCase
+  def test_parse
+    io = StringIO.new("", "r+:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    io.write_uint32(0)
+    io.write_ascii("moov")
+    size = io.pos
+    io.pos = 0
+    io.write_uint32(size)
+    io.pos = 0
+
+    box = BMFF::Box.get_box(io, nil)
+    assert_instance_of(BMFF::Box::Movie, box)
+    assert_equal(size, box.actual_size)
+    assert_equal("moov", box.type)
+    assert_equal([], box.children)
+    assert(box.container?)
+  end
+end
