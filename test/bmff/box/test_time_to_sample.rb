0d7f5023476085f86880e9e29afb17fdba977af4
Takayuki Ogiso <gomatofu@gmail.com>
2014-05-18 02:24:28 +0900

    Add a test file for BMFF::Box::TimeToSample.

---
+++ test/bmff/box/test_time_to_sample.rb
@@ -0,0 +1,36 @@
+# coding: utf-8
+# vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
+
+require_relative '../../minitest_helper'
+require 'bmff/box'
+require 'stringio'
+
+class TestBMFFBoxTimeToSample < MiniTest::Unit::TestCase
+  def test_parse
+    io = StringIO.new("", "r+:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    io.write_uint32(0)
+    io.write_ascii("stts")
+    io.write_uint8(0) # version
+    io.write_uint24(0) # flags
+    io.write_uint32(3) # entry_count
+    3.times do |i|
+      io.write_uint32(20 + i) # sample_count
+      io.write_uint32(10 + i) # sample_delta
+    end
+    size = io.pos
+    io.pos = 0
+    io.write_uint32(size)
+    io.pos = 0
+
+    box = BMFF::Box.get_box(io, nil)
+    assert_instance_of(BMFF::Box::TimeToSample, box)
+    assert_equal(size, box.actual_size)
+    assert_equal("stts", box.type)
+    assert_equal(0, box.version)
+    assert_equal(0, box.flags)
+    assert_equal(3, box.entry_count)
+    assert_equal([20, 21, 22], box.sample_count)
+    assert_equal([10, 11, 12], box.sample_delta)
+  end
+end
