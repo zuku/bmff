ae0ca2233f3ac11d9e68729c1c8dfda6c3efcb84
Takayuki Ogiso <gomatofu@gmail.com>
2014-05-18 02:28:22 +0900

    Add a test file for BMFF::Box::CompositionOffset.

---
+++ test/bmff/box/test_composition_offset.rb
@@ -0,0 +1,64 @@
+# coding: utf-8
+# vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
+
+require_relative '../../minitest_helper'
+require 'bmff/box'
+require 'stringio'
+
+class TestBMFFBoxCompositionOffset < MiniTest::Unit::TestCase
+  def test_parse_v0
+    io = StringIO.new("", "r+:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    io.write_uint32(0)
+    io.write_ascii("ctts")
+    io.write_uint8(0) # version
+    io.write_uint24(0) # flags
+    io.write_uint32(3) # entry_count
+    3.times do |i|
+      io.write_uint32(i) # sample_count
+      io.write_uint32(10 + i) # sample_offset
+    end
+    size = io.pos
+    io.pos = 0
+    io.write_uint32(size)
+    io.pos = 0
+
+    box = BMFF::Box.get_box(io, nil)
+    assert_instance_of(BMFF::Box::CompositionOffset, box)
+    assert_equal(size, box.actual_size)
+    assert_equal("ctts", box.type)
+    assert_equal(0, box.version)
+    assert_equal(0, box.flags)
+    assert_equal(3, box.entry_count)
+    assert_equal([0, 1, 2], box.sample_count)
+    assert_equal([10, 11, 12], box.sample_offset)
+  end
+
+  def test_parse_v1
+    io = StringIO.new("", "r+:ascii-8bit")
+    io.extend(BMFF::BinaryAccessor)
+    io.write_uint32(0)
+    io.write_ascii("ctts")
+    io.write_uint8(1) # version
+    io.write_uint24(0) # flags
+    io.write_uint32(3) # entry_count
+    3.times do |i|
+      io.write_uint32(i) # sample_count
+      io.write_int32(10 + i) # sample_offset
+    end
+    size = io.pos
+    io.pos = 0
+    io.write_uint32(size)
+    io.pos = 0
+
+    box = BMFF::Box.get_box(io, nil)
+    assert_instance_of(BMFF::Box::CompositionOffset, box)
+    assert_equal(size, box.actual_size)
+    assert_equal("ctts", box.type)
+    assert_equal(1, box.version)
+    assert_equal(0, box.flags)
+    assert_equal(3, box.entry_count)
+    assert_equal([0, 1, 2], box.sample_count)
+    assert_equal([10, 11, 12], box.sample_offset)
+  end
+end
