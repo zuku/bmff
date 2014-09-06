d109269b361b5b44a02a9a23bcc56472db3fc682
Takayuki Ogiso <gomatofu@gmail.com>
2014-09-06 17:55:28 +0900

    Add settings for Coveralls. #4

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,6 +1,9 @@
 # coding: utf-8
 # vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 autoindent:
 
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bmff'
 
